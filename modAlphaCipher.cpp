087b3ae8b2641c7c7b5d87f364cf577d2b491e1f
alexeymarinin <81802758+alexeymarinin@users.noreply.github.com>
2021-04-23 19:51:58 +0300

    Update modAlphaCipher.cpp

--- modAlphaCipher.cpp
+++ modAlphaCipher.cpp
@@ -1,67 +1,67 @@
-# включить  " modAlphaCipher.h "
-modAlphaCipher :: modAlphaCipher ( сопзЬ строку & SKEY)
+#include "modAlphaCipher.h"
+modAlphaCipher::modAlphaCipher(const string& skey)
 {
-    локаль loc ( " ru_RU.UTF-8 " ); // русская локаль для корректной смены регистратора
-    wstring_convert <codecvt_utf8 < wchar_t >, wchar_t > кодек; // кодек UTF-8
-    wstring ws = кодек. from_bytes (numAlpha); // перекодируем
-    for ( unsigned i = 0 ; i <ws. size (); i ++) {
-        alphaNum [ws [i]] = i;
+    locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
+    wstring ws = codec.from_bytes(numAlpha); // перекодируем
+    for (unsigned i=0; i<ws.size(); i++) {
+        alphaNum[ws[i]]=i;
     }
-    ключ = преобразовать (скей);
+    key = convert(skey);
 }
 
-Строка modAlphaCipher :: шифровать ( сопзЬ строку & open_text)
+string modAlphaCipher::encrypt(const string& open_text)
 {
-   vector < int > work = convert (open_text);
-    for ( unsigned i = 0 ; i <work. size (); i ++) {
-        work [i] = (work [i] + key [i% key. size ()])% alphaNum. размер ();
+   vector<int> work = convert(open_text);
+    for(unsigned i=0; i < work.size(); i++) {
+        work[i] = (work[i] + key[i % key.size()]) % alphaNum.size();
     }
-    return  convert (работа);
+    return convert(work);
 }
 
-Строка modAlphaCipher :: расшифровывать ( Const строка & cipher_text)
+string modAlphaCipher::decrypt(const string& cipher_text)
 {
-    vector < int > work = convert (cipher_text);
-    for ( unsigned i = 0 ; i <work. size (); i ++) {
-        work [i] = (work [i] + alphaNum. size () - key [i% key. size ()])% alphaNum. размер ();
+    vector<int> work = convert(cipher_text);
+    for(unsigned i=0; i < work.size(); i++) {
+        work[i] = (work[i] + alphaNum.size() - key[i % key.size()]) % alphaNum.size();
     }
-    return  convert (работа);
+    return convert(work);
 }
 
-встроенный вектор < int > modAlphaCipher :: convert ( const string & s)
+inline vector<int> modAlphaCipher::convert(const string& s)
 {
-   vector < int > result;
-    локаль loc ( " ru_RU.UTF-8 " ); // русская локаль для корректной смены регистратора
-    wstring_convert <codecvt_utf8 < wchar_t >, wchar_t > кодек; // кодек UTF-8
-    wstring ws = кодек. from_bytes (s); // перекодируем
-    for ( unsigned i = 0 ; i <ws. size (); i ++) {
-        результат. push_back (alphaNum [WS [i]]);
+   vector<int> result;
+    locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
+    wstring ws = codec.from_bytes(s); // перекодируем
+    for (unsigned i=0; i<ws.size(); i++) {
+        result.push_back(alphaNum[ws[i]]);
     }
-    вернуть результат;
+    return result;
 }
 
-встроенная строка modAlphaCipher :: convert ( const vector < int > & v)
+inline string modAlphaCipher::convert(const vector<int>& v)
 {
-    строковый результат;
-    локаль loc ( " ru_RU.UTF-8 " ); // русская локаль для корректной смены регистратора
-    wstring_convert <codecvt_utf8 < wchar_t >, wchar_t > кодек; // кодек UTF-8
-    wstring ws = кодек. from_bytes (numAlpha);
-    wstring result_s = кодек. from_bytes ( " " );
-    for ( unsigned i = 0 ; i <v. size (); i ++) {
-        полученные результаты. push_back (ws [v [i]]);
+    string result;
+    locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
+    wstring ws = codec.from_bytes(numAlpha);
+    wstring result_s = codec.from_bytes("");
+    for (unsigned i=0; i<v.size(); i++) {
+        result_s.push_back(ws[v[i]]);
     }
-    результат = кодек. to_bytes (результат_с);
-    вернуть результат;
+    result = codec.to_bytes(result_s);
+    return result;
 }
 
-    std :: string result;
-    локаль loc ( " ru_RU.UTF-8 " ); // русская локаль для корректной смены регистратора
-    wstring_convert <codecvt_utf8 < wchar_t >, wchar_t > кодек; // кодек UTF-8
-    wstring ws = codec.from_bytes (numAlpha);
-    wstring result_s = codec.from_bytes ( " " );
-    for ( unsigned i = 0 ; i <v.size (); i ++) {
-        полученные результаты. push_back (ws [v [i]]);
+    std::string result;
+    locale loc("ru_RU.UTF-8"); // русская локаль для корректной смены регистра
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; //кодек UTF-8
+    wstring ws = codec.from_bytes(numAlpha);
+    wstring result_s = codec.from_bytes("");
+    for (unsigned i=0; i<v.size(); i++) {
+        result_s.push_back(ws[v[i]]);
     }
-    результат = codec.to_bytes (результат_с);
-    вернуть результат;
+    result = codec.to_bytes(result_s);
+    return result;
 }
